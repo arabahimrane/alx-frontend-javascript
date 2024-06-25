40192d69207a0a4e60e63c824ac13439c5d14a8b
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-06-24 20:14:48 -0400

    0-constat

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,2 @@
-import { taskFirst, taskNext } from './0-constants.js';
-
+import { taskFirst, taskNext } from '/alx-backend-javascript/0x00-ES6_basic/0-constants.js';
 console.log(`${taskFirst()} ${taskNext()}`);
